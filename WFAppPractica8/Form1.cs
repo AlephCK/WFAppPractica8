6d7eae775072630622aec9c3b3c2c42bd36bcbf6
AlephCK <alephck@outlook.com>
2019-07-31 19:57:53 -0400

    Practica Finalizada

---
+++ WFAppPractica8/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFAppPractica8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+  
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string ID_Factura = tbx_idfactura.Text;
+            string Condiciones = tbx_condiciones.Text;
+            string ID_Cliente = tbx_idcliente.Text;
+            string Fecha = tbx_fecha.Text;
+            string Monto = tbx_monto.Text;
+            string Estado = cmb_estado.Text;
+
+
+            string[] arr = { ID_Factura, Condiciones, ID_Cliente, Fecha, Monto, Estado };
+            
+            var process = new Process();
+            process.StartInfo.FileName = Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.FullName, "caFactura/bin/Debug/caFactura.exe");
+            process.StartInfo.Arguments = arr[2];  
+            process.Start();
+
+        }
+    }
+}
---
+++ caFactura/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caFactura
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args[0]);
+            Console.ReadKey();
+
+            var p = new Process();
+            string filename = Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.FullName, args[0] + ".jpg");
+            p.StartInfo = new ProcessStartInfo(filename)
+            {
+                UseShellExecute = true
+            };
+            p.Start();
+        }
+    }
+}
